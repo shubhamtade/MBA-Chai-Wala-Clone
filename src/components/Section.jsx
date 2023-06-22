f3b15158da9c758bf836e605dad38c48826fe4c3
shubhamtade <shubham.tade@gmail.com>
2023-06-21 21:09:34 -0700

    animation add

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,4 @@
-import { color } from "framer-motion";
+import { color, motion } from "framer-motion";
 import React from "react";
 
 const Section = ({
@@ -13,22 +13,87 @@ const Section = ({
   btnBgColor,
   btnColor,
 }) => {
+
+  
+  const headingOption = {
+    initial: {
+      y: "-100%",
+      opacity: 0,
+    },
+    whileInView: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  const textOptions = {
+    ...headingOption,
+    transition: {
+      delay: 0.3,
+    },
+  };
+
+  const btnOptions = {
+    initial: {
+      y: "100%",
+      opacity: 0,
+    },
+    whileInView: {
+      y: 0,
+      opacity: 1,
+    },
+    transition: {
+      delay: 0.3,
+      ease: "easeIn",
+    },
+  };
+
+  const imgOptions = {
+    initial: {
+      scale: 0.1,
+      opacity: 0,
+    },
+    whileInView: {
+      scale: 1,
+      opacity: 1,
+    },
+    transition: {
+      delay: 0.3,
+    },
+  };
+
   return (
     <>
       <section className="section" style={{ backgroundColor: backgroundColor }}>
         <div>
-          <h3 style={{ color: headingColor }} data-cursorpointer={true}>{h3}</h3>
-          <p style={{ color: textColor }} data-cursorpointer={true}>{text}</p>
+          <motion.h3
+            style={{ color: headingColor }}
+            data-cursorpointer={true}
+            {...headingOption}
+          >
+            {h3}
+          </motion.h3>
+          <motion.p
+            style={{ color: textColor }}
+            data-cursorpointer={true}
+            {...textOptions}
+          >
+            {text}
+          </motion.p>
 
           {btnText && (
-            <button style={{ backgroundColor: btnBgColor, color: btnColor }} data-cursorpointer={true}>
+            <motion.button
+              style={{ backgroundColor: btnBgColor, color: btnColor }}
+              data-cursorpointer={true}
+              {...btnOptions}
+            >
               {btnText}
-            </button>
+            </motion.button>
           )}
 
-          <div>
+          <motion.div {...imgOptions}>
             <img src={imgSrc} alt="ImgSrc" style={{ width: imgSize }} />
-          </div>
+          </motion.div>
         </div>
       </section>
     </>
